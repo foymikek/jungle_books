ca6f979db749fe5e8cb5376764d25e7ebe6abd1e
foymikek <foymikek@gmail.com>
2021-04-18 18:10:34 -0600

    Add refactored index test.

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Books API', type: :request do
   describe 'index testing' do
-      it 'should return all books' do
-        2.times do
+     2.times do
           FactoryBot.create(:book)
         end
-        
-        get '/api/v1/books'
-        
-        expect(response).to have_http_status(:success)
-        
-        expect(JSON.parse(response.body).size).to eq(2)
+    it 'should return all books' do
+      get '/api/v1/books'
+      
+      expect(response).to have_http_status(:success)
+      
+      expect(JSON.parse(response.body).size).to eq(2)
    end
   end
 
