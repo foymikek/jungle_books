6896e3c8af401e5e01980b4d6fee7ee8c6005ac7
foymikek <foymikek@gmail.com>
2021-04-20 10:40:53 -0600

    Add passing and refactored tests.

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Books API', type: :request do
   describe 'index testing' do
-    Book.destroy_all
-    Author.destroy_all
-
-    author = Author.create(
-      first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample 
-      )
-
-    2.times do
-         author.books.create(title: Faker::Book.title, author_id: author.id)
-       end
-
+    
     it 'should return all books' do
+      Book.destroy_all
+      Author.destroy_all
+  
+      author = Author.create(
+        first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample 
+        )
+  
+      2.times do
+           author.books.create(title: Faker::Book.title, author_id: author.id)
+         end
       get '/api/v1/books'
       
       expect(response).to have_http_status(:success)
@@ -39,25 +39,30 @@ RSpec.describe 'Books API', type: :request do
       expect(Author.all.count).to eq(1)
     end
 
-    it 'responds with unprocessable_entity sad path with author initials' do
-      post '/api/v1/books', params: {book: { title: Faker::Book.title, author: "AB"} }
-
-      expect(response).to have_http_status(:unprocessable_entity)
-
-    end
-
     it 'responds with unprocessable_entity sad path with inconplete title' do
-      post '/api/v1/books', params: {book: { title: 'ab', author: Faker::Book.author} }
+      Book.destroy_all
+      Author.destroy_all
+
+      post '/api/v1/books', params: {
+        book: { title: 'ab'},
+        author: { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample }
+      }
 
       expect(response).to have_http_status(:unprocessable_entity)
-
     end
   end
 
   describe 'delete testing' do
-    let!(:book) { FactoryBot.create(:book) }
-
     it 'deletes a book record' do
+      Book.destroy_all
+      Author.destroy_all
+  
+      author = Author.create(
+          first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample 
+          )
+    
+      book = author.books.create(title: Faker::Book.title, author_id: author.id)
+
       expect {
         delete "/api/v1/books/#{book.id}"
       }.to change { Book.count }.from(1).to(0)
