7797ae3ac6846aaefdedd06c55def2db3ec149df
foymikek <foymikek@gmail.com>
2021-04-20 10:41:45 -0600

    Add refactored book_params, removed author field.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -28,7 +28,7 @@ module Api
       private
     
       def book_params
-        params.require(:book).permit(:title, :author)
+        params.require(:book).permit(:title)
       end
 
       def author_params
