9929da21f800c04b9a2d1b120123512193e99318
foymikek <foymikek@gmail.com>
2021-04-20 10:16:04 -0600

    Add refactored and passing create functionality and test. Authort and book record created.

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Books API', type: :request do
   describe 'index testing' do
-     2.times do
-          FactoryBot.create(:book)
-        end
+    Book.destroy_all
+    Author.destroy_all
+
+    author = Author.create(
+      first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample 
+      )
+
+    2.times do
+         author.books.create(title: Faker::Book.title, author_id: author.id)
+       end
+
     it 'should return all books' do
       get '/api/v1/books'
       
@@ -15,12 +23,20 @@ RSpec.describe 'Books API', type: :request do
   end
 
   describe 'create testing' do
+    Book.destroy_all
+    Author.destroy_all
+
     it 'can create a new book record' do
+      expect(Author.all.count).to eq(0)
       expect {
-        post '/api/v1/books', params: {book: { title: Faker::Book.title, author: Faker::Book.author} }
-  
+        post '/api/v1/books', params: {
+          book:   { title: Faker::Book.title },
+          author: { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: (15..85).to_a.sample }
+        }
         expect(response).to have_http_status(:created)
       }.to change { Book.count }.from(0).to(1)
+
+      expect(Author.all.count).to eq(1)
     end
 
     it 'responds with unprocessable_entity sad path with author initials' do
